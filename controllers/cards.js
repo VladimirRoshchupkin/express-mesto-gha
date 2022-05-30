a3e6d04bad8418414fb52371bd48cf6d26be03d8
Vladimir Roshchupkin <vladimir.roschupkin@rusvinyl.ru>
2022-05-30 08:38:21 +0300

    after action 33

--- controllers/cards.js
+++ controllers/cards.js
@@ -38,7 +38,6 @@ const deleteCard = (req, res) => {
       if (err.kind === 'ObjectId') {
         return res.status(400).send({ message: 'Id is not correct' });
       }
-      console.log(err);
       return res.status(500).send({ message: 'Server error' });
     });
 };
