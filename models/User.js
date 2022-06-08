371fecbcd67319d924cf31c3569571dc42d7269b
Vladimir Roshchupkin <vladimir.roschupkin@rusvinyl.ru>
2022-06-08 08:19:09 +0300

    PR14 with auth and custom schem validation test

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-// unction avatarValidator(val) { // нашел здесь https://mongoosejs.com/docs/api.html#schematype_SchemaType-validate
-//  const req = /https?:\/\/(www\.)?[0-9a-z-.]*\.[a-z-.]{2,}([0-9a-z-._~:/?#[\]@!$&'()*+,;=])*#*$/i;
-//  return reqexp.test(val);
-// }
+function avatarValidator(val) { // нашел здесь https://mongoosejs.com/docs/api.html#schematype_SchemaType-validate
+  const reg = /https?:\/\/(www\.)?[0-9a-z-.]*\.[a-z-.]{2,}([0-9a-z-._~:/?#[\]@!$&'()*+,;=])*#*$/i;
+  return reg.test(val);
+}
+const custom = [avatarValidator, 'Uh oh, {PATH} does not equal "something".'];
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,9 +26,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     // required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    // validate: {
-  // avatarValidator,
-  // message: 'avatar URL validation error',
+    validate: custom, // {
+    // avatarValidator,
+    // message: 'avatar URL validation error',
     // }, // все делают через npm-валидатор, я сделать по заданию, чувствую опять проблемы
   },
   email: {
